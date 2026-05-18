4a52af6cff6dcba035f14dbc86eb255cdb851ff8
eswarprasad1898 <t.eswarprasad369@gmail.com>
2026-05-18 18:28:39 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -28,7 +28,7 @@ schedule = [
     {"team1": "SRH", "team2": "KKR", "date": "2026-05-03"},
     {"team1": "GT", "team2": "PBKS", "date": "2026-05-03"},
     {"team1": "MI", "team2": "LSG", "date": "2026-05-04"},
-    {"team1": "CSK", "team2": "SRH, "date": "2026-05-18"},
+    {"team1": "CSK", "team2": "SRH", "date": "2026-05-18"},
 ]
 
 # ===== FUNCTIONS =====
